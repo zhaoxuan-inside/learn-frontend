6a402eafac2a1867fce1117866fbfbf104657056
hardstone <zhaoxuanmail@163.com>
2026-04-05 19:48:32 +0800

    [Feat] type translate

---
+++ javascript/_051_type_translate.js
@@ -0,0 +1,12 @@
+function num2str (num) {
+    console.log(typeof(num));
+    console.log(num);
+    let val = String(num);
+    console.log(typeof(val));
+    console.log(val);
+}
+
+
+
+
+num2str(10);
