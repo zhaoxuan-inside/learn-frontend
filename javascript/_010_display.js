9a48106a39eed9abbf256f92dc9db2dfc15f72b5
hardstone <zhaoxuanmail@163.com>
2026-04-05 17:37:37 +0800

    [Feat] js operator === and !===

--- javascript/_010_display.js
+++ javascript/_010_display.js
@@ -1,11 +1,11 @@
-function say_hello() {
-    document.getElementById("Alice").innerHTML="Alice: hello, world!"
-}
-
 function display_datetime() {
-    document.write(Date())
+    document.writeln(Date())
 }
 
 function window_alert() {
     window.alert("Warning FBI")
+}
+
+function display_ele(ele_name, ...content) {
+    document.getElementById(ele_name).innerHTML = content;
 }
---
+++ javascript/_040_operator.js
@@ -0,0 +1,13 @@
+function compare(var_1, var_2) {
+    
+    if (var_1 === var_2) {
+        return "type equal, value equal."
+    }
+
+    if (var_1 !== var_2) {
+        return "type not equal, value not queal."
+    }
+
+    return "other"
+
+}
